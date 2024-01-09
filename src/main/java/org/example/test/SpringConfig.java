84e885dec18ca9d667d8c71776f41c2e8a7f723c
moon <cogus2010@naver.com>
2024-01-10 00:35:01 +0900

    2024-01-10 JDBC 연동

--- src/main/java/org/example/test/SpringConfig.java
+++ src/main/java/org/example/test/SpringConfig.java
@@ -1,13 +1,23 @@
 package org.example.test;
 
+import org.example.test.repository.JdbcMemberRepository;
 import org.example.test.repository.MemberRepository;
 import org.example.test.repository.MemoryMemberRepository;
 import org.example.test.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//      return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
